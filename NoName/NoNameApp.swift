f7054f9f611461c9d36aafbeb88851dd096aa7f6
takami.nakajima <takami.nakajima@gmail.com>
2024-03-22 23:16:43 +0900

    Initial Commit

---
+++ NoName/NoNameApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoNameApp.swift
+//  NoName
+//
+//  Created by 中島昂海 on 2024/03/22.
+//
+
+import SwiftUI
+
+@main
+struct NoNameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
