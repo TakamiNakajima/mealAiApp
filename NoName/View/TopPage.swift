b6e11ef96ef9cbb6cce94ee18ecb1c940524627d
takami.nakajima <takami.nakajima@gmail.com>
2024-04-03 19:56:32 +0900

    トップ画面デザイン修正

--- NoName/View/TopPage.swift
+++ NoName/View/TopPage.swift
@@ -14,58 +14,75 @@ struct TopPage: View {
     @EnvironmentObject var manager: StepRepository
     var body: some View {
         if let user = authViewModel.currentUser {
-            List {
-                Section {
-                    HStack {
-                        Text(user.initials)
-                            .font(.title)
-                            .fontWeight(.semibold)
+            VStack() {
+                ZStack(alignment: .bottomLeading) {
+                    Color(Color(red: 70/255, green: 158/255, blue: 208/255))
+                        .ignoresSafeArea()
+                    ZStack(alignment: .leading) {
+                        Rectangle()
                             .foregroundColor(.white)
-                            .frame(width: 72, height: 72)
-                            .background(Color(.systemGray3))
-                            .clipShape(Circle())
-                        VStack(alignment: .leading, spacing: 4){
-                            Text(user.fullname)
-                                .font(.subheadline)
+                            .cornerRadius(10)
+                            .shadow(radius: 5)
+                        HStack {
+                            Text(user.initials)
+                                .font(.title)
                                 .fontWeight(.semibold)
-                                .padding(.top, 4)
-                            Text(user.email)
-                                .font(.footnote)
-                                .foregroundColor(.gray)
-                        }
-                    }
-                }
-                Section("アカウント") {
-                    Button {
-                        authViewModel.signOut()
-                    } label: {
-                        SettingsRow(
-                            imageName: "arrow.left.circle.fill",
-                            title: "Sign Out",
-                            tintColor: Color(.red)
-                        )
-                    }
-                }
-                Section("今日の歩数  (\(topPageViewModel.stepData.timeStamp.displayDateString())更新)") {
-                    HStack {
-                        Text(topPageViewModel.stepData.step.formattedString())
-                            .font(.system(size: 16))
-                        Spacer()
-                        Button(action: {
-                            Task {
-                                try await topPageViewModel.fetchAndSaveSteps(uid: authViewModel.currentUser!.id)
+                                .foregroundColor(.white)
+                                .frame(width: 64, height: 64)
+                                .background(Color(.systemGray3))
+                                .clipShape(Circle())
+                            VStack(alignment: .leading, spacing: 4){
+                                Text(user.fullname)
+                                    .font(.subheadline)
+                                    .fontWeight(.semibold)
+                                    .padding(.top, 4)
+                                Text(user.email)
+                                    .font(.footnote)
+                                    .foregroundColor(.gray)
                             }
-                        }) {
-                            Image(systemName: "arrow.clockwise")
+                            Spacer()
+                            VStack {
+                                Text(topPageViewModel.stepData.step.formattedString())
+                                    .font(.system(size: 24))
+                                    .fontWeight(.bold)
+
+                                Text("(\(topPageViewModel.stepData.timeStamp.displayDateString())更新)")
+                                    .font(.system(size: 12))
+                                    .foregroundColor(.gray)
+                                    .fontWeight(.light)
+                            }
+                            Button(action: {
+                                Task {
+                                    try await topPageViewModel.fetchAndSaveSteps(uid: authViewModel.currentUser!.id)
+                                }
+                            }) {
+                                Image(systemName: "arrow.clockwise")
+                            }
+                            .disabled(!topPageViewModel.isButtonEnabled)
                         }
-                        .disabled(!topPageViewModel.isButtonEnabled)
+                        .padding()
+                    }
+                    .offset(x: 0, y: 32)
+                    .padding(.horizontal, 24)
+                    .onAppear {
+                        Task {
+                            try await topPageViewModel.fetchAndSaveSteps(uid: authViewModel.currentUser!.id)
+                        }
                     }
                 }
-                .onAppear {
-                    Task {
-                        try await topPageViewModel.fetchAndSaveSteps(uid: authViewModel.currentUser!.id)
-                    }
-                }
+                .frame(height: 96)
+                Spacer()
+//                Section("アカウント") {
+//                    Button {
+//                        authViewModel.signOut()
+//                    } label: {
+//                        SettingsRow(
+//                            imageName: "arrow.left.circle.fill",
+//                            title: "Sign Out",
+//                            tintColor: Color(.red)
+//                        )
+//                    }
+//                }
             }
         }
     }
