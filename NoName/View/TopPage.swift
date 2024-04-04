1715e37822f28d036cfeea42b898d172600fa39e
takami.nakajima <takami.nakajima@gmail.com>
2024-04-04 18:54:05 +0900

    TopPageのリファクタとテーマカラー追加

--- NoName/View/TopPage.swift
+++ NoName/View/TopPage.swift
@@ -14,76 +14,85 @@ struct TopPage: View {
     @EnvironmentObject var manager: StepRepository
     var body: some View {
         if let user = authViewModel.currentUser {
-            VStack() {
-                ZStack(alignment: .bottomLeading) {
-                    Color(Color(red: 70/255, green: 158/255, blue: 208/255))
-                        .ignoresSafeArea()
-                    ZStack(alignment: .leading) {
-                        Rectangle()
-                            .foregroundColor(.white)
-                            .cornerRadius(10)
-                            .shadow(radius: 5)
-                        HStack {
-                            Text(user.initials)
-                                .font(.title)
-                                .fontWeight(.semibold)
-                                .foregroundColor(.white)
-                                .frame(width: 64, height: 64)
-                                .background(Color(.systemGray3))
-                                .clipShape(Circle())
-                            VStack(alignment: .leading, spacing: 4){
-                                Text(user.fullname)
-                                    .font(.subheadline)
-                                    .fontWeight(.semibold)
-                                    .padding(.top, 4)
-                                Text(user.email)
-                                    .font(.footnote)
-                                    .foregroundColor(.gray)
-                            }
-                            Spacer()
-                            VStack {
-                                Text(topPageViewModel.stepData.step.formattedString())
-                                    .font(.system(size: 24))
-                                    .fontWeight(.bold)
-
-                                Text("(\(topPageViewModel.stepData.timeStamp.displayDateString())更新)")
-                                    .font(.system(size: 12))
-                                    .foregroundColor(.gray)
-                                    .fontWeight(.light)
-                            }
-                            Button(action: {
-                                Task {
-                                    try await topPageViewModel.fetchAndSaveSteps(uid: authViewModel.currentUser!.id)
-                                }
-                            }) {
-                                Image(systemName: "arrow.clockwise")
-                            }
-                            .disabled(!topPageViewModel.isButtonEnabled)
-                        }
-                        .padding()
-                    }
-                    .offset(x: 0, y: 32)
-                    .padding(.horizontal, 24)
-                    .onAppear {
-                        Task {
-                            try await topPageViewModel.fetchAndSaveSteps(uid: authViewModel.currentUser!.id)
-                        }
-                    }
+            ZStack {
+                // 背景色
+                VStack(spacing: 0) {
+                    Color.customThemeColor
+                        .frame(height: 144)
+                    Color.white
                 }
-                .frame(height: 96)
-                Spacer()
-//                Section("アカウント") {
-//                    Button {
-//                        authViewModel.signOut()
-//                    } label: {
-//                        SettingsRow(
-//                            imageName: "arrow.left.circle.fill",
-//                            title: "Sign Out",
-//                            tintColor: Color(.red)
-//                        )
-//                    }
-//                }
+                .edgesIgnoringSafeArea(.all)
+                
+                // アカウント情報
+                VStack(alignment: .leading) {
+                    Spacer()
+                        .frame(height: 16)
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(Color.white)
+                        .frame(height: 100)
+                        .shadow(radius: 5)
+                        .overlay(
+                            HStack {
+                                Text(user.initials)
+                                    .font(.title)
+                                    .fontWeight(.semibold)
+                                    .foregroundColor(.white)
+                                    .frame(width: 64, height: 64)
+                                    .background(Color(.systemGray3))
+                                    .clipShape(Circle())
+                                VStack(alignment: .leading) {
+                                    Text(user.fullname)
+                                        .font(.subheadline)
+                                        .fontWeight(.semibold)
+                                        .padding(.top, 4)
+                                    Text(user.email)
+                                        .font(.footnote)
+                                        .foregroundColor(.gray)
+                                }
+                                Spacer()
+                                VStack {
+                                    Text(topPageViewModel.stepData.step.formattedString())
+                                        .font(.system(size: 24))
+                                        .fontWeight(.bold)
+                                    
+                                    Text("(\(topPageViewModel.stepData.timeStamp.displayDateString())更新)")
+                                        .font(.system(size: 12))
+                                        .foregroundColor(.gray)
+                                        .fontWeight(.light)
+                                }
+                                Button(action: {
+                                    Task {
+                                        try await topPageViewModel.fetchAndSaveSteps(uid: authViewModel.currentUser!.id)
+                                    }
+                                }) {
+                                    Image(systemName: "arrow.clockwise")
+                                }
+                                .disabled(!topPageViewModel.isButtonEnabled)
+                                
+                            }
+                                .padding()
+                                .onAppear {
+                                    Task {
+                                        try await topPageViewModel.fetchAndSaveSteps(uid: authViewModel.currentUser!.id)
+                                    }
+                                }
+                        )
+                        .padding()
+                    Spacer()
+                }
             }
+            //                Section("アカウント") {
+            //                    Button {
+            //                        authViewModel.signOut()
+            //                    } label: {
+            //                        SettingsRow(
+            //                            imageName: "arrow.left.circle.fill",
+            //                            title: "Sign Out",
+            //                            tintColor: Color(.red)
+            //                        )
+            //                    }
+            //                }
+            //            }
         }
     }
 }
---
+++ NoName/extension/Color.swift
@@ -0,0 +1,6 @@
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let customThemeColor = Color("ThemeColor")
+}
