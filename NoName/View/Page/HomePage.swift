50cb739308a66177f47ee61266cde2cc88ee4efe
takami.nakajima <takami.nakajima@gmail.com>
2024-10-08 20:47:28 +0900

    ホーム画面デザイン修正

--- NoName/View/Page/HomePage.swift
+++ NoName/View/Page/HomePage.swift
@@ -16,7 +16,7 @@ struct HomePage: View {
                 
                 Spacer()
                     .frame(height: 8)
-
+                
                 HStack {
                     DashedCircleText(number: "23")
                     DashedCircleText(number: "24")
@@ -30,76 +30,50 @@ struct HomePage: View {
                 Spacer()
                     .frame(height: 8)
                 
-//                Text("9月29日")
-//                    .font(.system(size: 16))
-//                    .fontWeight(.semibold)
-//                    .foregroundColor(.white)
-//                    .frame(maxWidth: .infinity, minHeight: 26)
-//                    .background(Color.blue)
-//                    .multilineTextAlignment(.center)
-                
-                List {
-                    
-                    Section("カロリー") {
-                        HStack {
-                            Text(user.initials)
-                                .font(.title)
-                                .fontWeight(.semibold)
-                                .foregroundColor(.white)
-                                .frame(width: 72, height: 72)
-                                .background(Color(.systemGray3))
-                                .clipShape(Circle())
-                            VStack(alignment: .leading, spacing: 4){
-                                Text(user.fullname)
-                                    .font(.subheadline)
-                                    .fontWeight(.semibold)
-                                    .padding(.top, 4)
-                                Text(user.email)
-                                    .font(.footnote)
-                                    .foregroundColor(.gray)
+                HStack(spacing: 24) {
+                    HomeConteiner(title: "歩数", value: "\(manager.stepCount)歩")
+                        .onAppear {
+                            Task {
+                                await manager.fetchTodaySteps(uid: authViewModel.currentUser!.id)
                             }
                         }
-                    }
-                    Section("運動") {
-                        HStack {
-                            Text(manager.stepCount)
-                            Spacer()
-                            Button(action: {
-                                Task {
-                                    await manager.fetchTodaySteps(uid: authViewModel.currentUser!.id)
-                                }
-                            }) {
-                                Image(systemName: "arrow.clockwise")
-                            }
-                        }
-                    }
-                    Section("食事") {
-                        Button {
-                            authViewModel.signOut()
-                        } label: {
-                            SettingsRowView(
-                                imageName: "arrow.left.circle.fill",
-                                title: "Sign Out",
-                                tintColor: Color(.red)
-                            )
-                        }
-                        Button {
-                            print("Delete account..")
-                        } label: {
-                            SettingsRowView(
-                                imageName: "xmark.circle.fill",
-                                title: "Delete Account",
-                                tintColor: Color(.red)
-                            )
-                        }
-                    }
+                    HomeConteiner(title: "消費カロリー", value: "300kcal")
                 }
-                .onAppear {
-                    Task {
-                        await manager.fetchTodaySteps(uid: authViewModel.currentUser!.id)
-                    }
+                .padding()
+            }
+        }
+    }
+}
+
+struct HomeConteiner: View {
+    var title: String
+    var value: String
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 10)
+                .fill(Color(UIColor.systemGray6))
+                .frame(width: 160, height: 100)
+            VStack {
+                HStack {
+                    Text(title)
+                        .font(.subheadline)
+                        .padding(5)
+                        .foregroundColor(.gray)
+
+                    Spacer()
+                }
+                Spacer()
+                HStack {
+                    Spacer()
+                    Text(value)
+                        .font(.title)
+                        .padding(5)
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                        .fontWeight(.bold)
                 }
             }
+            .frame(width: 160, height: 100)
         }
     }
 }
